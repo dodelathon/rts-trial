ea721934bcfb91519f24d9c61232d16680fd57b8
dodelathon <36057018+dodelathon@users.noreply.github.com>
2019-01-24 02:00:33 +0000

    beginning

---
+++ Assets/Scripts/FollowUnit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowUnit : MonoBehaviour
+{
+    public Transform t;
+	
+
+	void Start ()
+    {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private Vector3 mPos;
+    private Rigidbody rb;
+    private Vector3 direction;
+    public float movespeed;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            ChangePos();
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
+        
+	}
+
+    void ChangePos()
+    {
+        Debug.Log(Input.mousePosition);
+        mPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = (mPos - transform.position).normalized;
+        rb.velocity = new Vector3(direction.x * movespeed, direction.y * movespeed, 0);
+    }
+}
